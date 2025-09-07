b0514fd47ecae63274a49094bd1dcb4440b7a7da
Amulya Jain <amuly.jain@gmail.com>
2025-09-07 18:04:16 -0400

    Add logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from .forms import CustomUserCreationForm, CustomErrorList
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect("home.index")
 
 
 def login(request):
